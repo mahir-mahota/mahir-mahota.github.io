d760547444f650d9da4fa0125f166ce61c34177e
Mahir Mahota <mahirmahota@gmail.com>
2023-06-02 16:02:47 -0400

    navbar disapears on scroll

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ nav_elements.forEach((element) => {
     bars.click();
   });
 });
+
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+};
