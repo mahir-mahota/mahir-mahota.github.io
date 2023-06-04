b74e0ba3dff5af88225659921ae333a0b78f2b43
Mahir Mahota <mahirmahota@gmail.com>
2023-06-04 16:20:54 -0400

    added animations and effects

--- script.js
+++ script.js
@@ -27,3 +27,15 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((element) => observer.observe(element));
