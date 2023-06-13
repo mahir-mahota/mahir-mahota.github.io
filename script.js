88f8c2ae9e872f934cdc04de3a99deb72a615595
Mahir Mahota <mahirmahota@gmail.com>
2023-06-13 12:51:42 -0400

    Added project card navigation functionality

--- script.js
+++ script.js
@@ -12,14 +12,13 @@ const nav_elements = document.querySelectorAll(".text, .button-container");
 nav_elements.forEach((element) => {
   element.addEventListener("click", () => {
     bars.click();
-    nav.classList.add("inactive");
   });
 });
 
-var prevScrollpos = window.pageYOffset;
+var prevScrollpos = window.scrollY;
 
 window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
+  var currentScrollPos = window.scrollY;
   if (prevScrollpos > currentScrollPos) {
     nav.style.top = "0";
   } else if (nav.classList.contains("inactive")) {
@@ -45,6 +44,12 @@ const project_title = document.querySelector(".project-title");
 const arrows = document.querySelectorAll(".arrow");
 const crosses = document.querySelectorAll(".project-close");
 
+const TOTAL_CARDS = 4;
+var current = 1;
+var current_string;
+var current_card;
+var current_card_string = "card";
+
 cards.forEach((card) => {
   card.addEventListener("click", () => {
     if (!card.classList.contains("expanded")) {
@@ -58,12 +63,14 @@ cards.forEach((card) => {
       arrows.forEach((arrow) => {
         arrow.classList.toggle("expanded");
       });
+      current_card = card;
+      current = parseInt(current_card.classList[2].substring(4, 5));
     }
   });
 });
 
-arrows.forEach((arrow) => {
-  arrow.addEventListener("click", () => {
+crosses.forEach((cross) => {
+  cross.addEventListener("click", () => {
     cards.forEach((card) => {
       if (card.classList.contains("expanded")) {
         card.classList.remove("expanded");
@@ -76,5 +83,32 @@ arrows.forEach((arrow) => {
         });
       }
     });
+  });
+});
+
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", () => {
+    current_card.classList.remove("expanded");
+    current_card.classList.add("non-focus");
+
+    if (arrow.classList.contains("arrow-left")) {
+      current = current == 1 ? TOTAL_CARDS : current - 1;
+    } else if (arrow.classList.contains("arrow-right")) {
+      current = current == TOTAL_CARDS ? 1 : current + 1;
+    }
+
+    current_card_string = current_card_string.substring(0, 4);
+    current_string = current.toString();
+
+    current_card_string = current_card_string.concat(current_string);
+
+    cards.forEach((card) => {
+      if (card.classList.contains(current_card_string)) {
+        current_card = card;
+      }
+    });
+
+    current_card.classList.add("expanded");
+    current_card.classList.remove("non-focus");
   });
 });
